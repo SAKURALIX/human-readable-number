6c72d2f95688a014b1dce6894f3a82ad1b7ae1e7
SAKURALIX <sakura.xlix@gmail.com>
2023-02-09 16:10:00 +0300

    feat: 100 percent completed

--- src/index.js
+++ src/index.js
@@ -9,40 +9,66 @@ module.exports = function toReadable (number) {
     let readableHundred
 
     let readableString
-    if (units === '0' && tens === 'udefined') {readableUnit = 'zero'}
-    if (units === '1') {readableUnit = 'one'}
-    if (units === '2') {readableUnit = 'two'}
-    if (units === '3') {readableUnit = 'three'}
-    if (units === '4') {readableUnit = 'four'}
-    if (units === '5') {readableUnit = 'five'}
-    if (units === '6') {readableUnit = 'six'}
-    if (units === '7') {readableUnit = 'seven'}
-    if (units === '8') {readableUnit = 'eight'}
-    if (units === '9') {readableUnit = 'nine'}
+    if (units === '0' && value.length > 1) {readableUnit = ''}
+    if (units === '0' && value.length === 1) {readableUnit = 'zero'}
+    if (units === '1' && tens != '1') {readableUnit = 'one'}
+    if (units === '1' && tens === '1') {readableUnit = ''}
+    if (units === '2' && tens != '1') {readableUnit = 'two'}
+    if (units === '2' && tens === '1') {readableUnit = ''}
+    if (units === '3' && tens != '1') {readableUnit = 'three'}
+    if (units === '3' && tens === '1') {readableUnit = ''}
+    if (units === '4' && tens != '1') {readableUnit = 'four'}
+    if (units === '4' && tens === '1') {readableUnit = ''}
+    if (units === '5' && tens != '1') {readableUnit = 'five'}
+    if (units === '5' && tens === '1') {readableUnit = ''}
+    if (units === '6' && tens != '1') {readableUnit = 'six'}
+    if (units === '6' && tens === '1') {readableUnit = ''}
+    if (units === '7' && tens != '1') {readableUnit = 'seven'}
+    if (units === '7' && tens === '1') {readableUnit = ''}
+    if (units === '8' && tens != '1') {readableUnit = 'eight'}
+    if (units === '8' && tens === '1') {readableUnit = ''}
+    if (units === '9' && tens != '1') {readableUnit = 'nine'}
+    if (units === '9' && tens === '1') {readableUnit = ''}
 
+    if (value.slice(-2) === '10') {readableTen = 'ten'}
+    if (value.slice(-2) === '11') {readableTen = 'eleven'}
+    if (value.slice(-2) === '12') {readableTen = 'twelve'}
+    if (value.slice(-2) === '13') {readableTen = 'thirteen'}
+    if (value.slice(-2) === '14') {readableTen = 'fourteen'}
+    if (value.slice(-2) === '15') {readableTen = 'fifteen'}
+    if (value.slice(-2) === '16') {readableTen = 'sixteen'}
+    if (value.slice(-2) === '17') {readableTen = 'seventeen'}
+    if (value.slice(-2) === '18') {readableTen = 'eighteen'}
+    if (value.slice(-2) === '19') {readableTen = 'nineteen'}
 
+    if (tens === undefined) {readableTen = ''}
+    if (tens === '0') {readableTen = ''}
     if (tens === '2') {readableTen = 'twenty '}
     if (tens === '3') {readableTen = 'thirty '}
-    if (tens === '4') {readableTen = 'forthy '}
+    if (tens === '4') {readableTen = 'forty '}
     if (tens === '5') {readableTen = 'fifty '}
     if (tens === '6') {readableTen = 'sixty '}
     if (tens === '7') {readableTen = 'seventy '}
     if (tens === '8') {readableTen = 'eighty '}
     if (tens === '9') {readableTen = 'ninety '}
 
+    if (hundreds === undefined) {readableHundred = ''}
+    if (hundreds === '1') {readableHundred = 'one hundred '}
+    if (hundreds === '2') {readableHundred = 'two hundred '}
+    if (hundreds === '3') {readableHundred = 'three hundred '}
+    if (hundreds === '4') {readableHundred = 'four hundred '}
+    if (hundreds === '5') {readableHundred = 'five hundred '}
+    if (hundreds === '6') {readableHundred = 'six hundred '}
+    if (hundreds === '7') {readableHundred = 'seven hundred '}
+    if (hundreds === '8') {readableHundred = 'eight hundred '}
     if (hundreds === '9') {readableHundred = 'nine hundred '}
 
-    if (tens === '9' && units === '0') {
-        readableTen = 'ninety ';
-        readableUnit = 'zero'
-    }
-
-
-
+    if (value.slice(-2) === '90') {readableTen = 'ninety '}
+    
     readableString = readableHundred + readableTen + readableUnit;
 
-    return readableString
-    // console.log(readableString)
+    return readableString.trim()
+    // console.log(readableString.trim())
     // return console.log(tens)
 }
 
@@ -52,4 +78,5 @@ module.exports = function toReadable (number) {
 // toReadable (56)
 // toReadable (79)
 
-// toReadable (999)
+// toReadable (990)
+// toReadable (0)
